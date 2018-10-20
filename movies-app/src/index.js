e092f96e099b7f6630dc2d1e26ff85e991be42b0
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-10-20 09:32:45 +0530

    Added ‘Login’ button on Header

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './screens/home/Home';
+import Header from './common/header/Header'
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
