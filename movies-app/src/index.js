9ba73bb7122358464c9d34d1dd55955ce3345ef6
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-10-20 17:45:35 +0530

    Added username and password inside Login form

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './screens/home/Home';
-import Header from './common/header/Header'
-
+import 'typeface-roboto';
 ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
